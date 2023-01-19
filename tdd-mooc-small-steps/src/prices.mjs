00e8f803c72541a9939dfc9372ced5e5644e4fb2
Petri Riihikallio <petri.riihikallio@metis.fi>
2023-01-19 18:56:47 +0200

    tcr: tests pass

--- tdd-mooc-small-steps/src/prices.mjs
+++ tdd-mooc-small-steps/src/prices.mjs
@@ -76,7 +76,7 @@ function createApp(database) {
   }
 
   function isMonday(date) {
-    return date.getDay() === 1;
+    return date.toTemporalInstant().toZonedDateTimeISO("UTC").toPlainDate().dayOfWeek === 1;
   }
 
   function isHoliday(date) {

0de9eb706b2965834492ed6b6aec2da257486685
Petri Riihikallio <petri.riihikallio@metis.fi>
2023-01-19 19:33:47 +0200

    tcr: tests pass

--- tdd-mooc-small-steps/src/prices.mjs
+++ tdd-mooc-small-steps/src/prices.mjs
@@ -83,8 +83,7 @@ function createApp(database) {
     const holidays = database.getHolidays();
     for (let row of holidays) {
       let holiday = Temporal.PlainDate.from(row.holiday);
-      if (
-        date && date.equals(holiday)
+      if (date && date.equals(holiday)
       ) {
         return true;
       }

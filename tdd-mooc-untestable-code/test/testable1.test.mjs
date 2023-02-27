214e714271017f3ade1fa93c09028bb0515cbacc
Petri Riihikallio <petri.riihikallio@metis.fi>
2023-02-27 21:21:45 +0200

    Add test for the original interface

--- tdd-mooc-untestable-code/test/testable1.test.mjs
+++ tdd-mooc-untestable-code/test/testable1.test.mjs
@@ -1,14 +1,20 @@
 import { expect } from "chai";
-import { daysUntilChristmas } from "../src/testable1.mjs";
+import { daysUntilChristmas, daysUntilChristmasWithParam } from "../src/testable1.mjs";
 
 describe("Testable 1: days until Christmas", () => {
   it("same year", () => {
     let date = new Date("2023-02-27T18:00:00")
-    expect(daysUntilChristmas(date)).to.equal(301);
+    expect(daysUntilChristmasWithParam(date)).to.equal(301);
   });
 
   it("next year", () => {
     let date = new Date("2025-12-28T18:00:00")
-    expect(daysUntilChristmas(date)).to.equal(362);
+    expect(daysUntilChristmasWithParam(date)).to.equal(362);
   });
+
+  it("original interface", () => {
+    expect(daysUntilChristmas()).to.be.a("number", "Not a number!");
+    expect(daysUntilChristmas()).to.be.within(0, 365, "Out of range!");
+  });
+  
 });

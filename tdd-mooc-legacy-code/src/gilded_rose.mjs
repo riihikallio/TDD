a73baeb08669520886925a3173b41489912f436e
Petri Riihikallio <petri.riihikallio@metis.fi>
2023-02-28 22:10:17 +0200

    tcr: tests pass

--- tdd-mooc-legacy-code/src/gilded_rose.mjs
+++ tdd-mooc-legacy-code/src/gilded_rose.mjs
@@ -17,16 +17,14 @@ export class Shop {
         if (this.items[i].quality > 0 && this.items[i].name != "Sulfuras, Hand of Ragnaros") {
           this.items[i].quality--;
         }
-      } else {
-        if (this.items[i].quality < 50) {
-          this.items[i].quality++;
-          if (this.items[i].name == "Backstage passes to a TAFKAL80ETC concert") {
-            if (this.items[i].sellIn < 11 && this.items[i].quality < 50) {
-              this.items[i].quality++;
-            }
-            if (this.items[i].sellIn < 6 && this.items[i].quality < 50) {
-              this.items[i].quality++;
-            }
+      } else if (this.items[i].quality < 50) {
+        this.items[i].quality++;
+        if (this.items[i].name == "Backstage passes to a TAFKAL80ETC concert") {
+          if (this.items[i].sellIn < 11 && this.items[i].quality < 50) {
+            this.items[i].quality++;
+          }
+          if (this.items[i].sellIn < 6 && this.items[i].quality < 50) {
+            this.items[i].quality++;
           }
         }
       }
@@ -37,13 +35,13 @@ export class Shop {
         if (this.items[i].name != "Aged Brie") {
           if (this.items[i].name != "Backstage passes to a TAFKAL80ETC concert") {
             if (this.items[i].quality > 0 && this.items[i].name != "Sulfuras, Hand of Ragnaros") {
-                this.items[i].quality--;
+              this.items[i].quality--;
             }
           } else {
             this.items[i].quality = 0;
           }
         } else if (this.items[i].quality < 50) {
-            this.items[i].quality++;
+          this.items[i].quality++;
         }
       }
     }

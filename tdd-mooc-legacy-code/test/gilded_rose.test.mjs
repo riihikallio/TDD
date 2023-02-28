7c0b76d3cdea3da06009f297a8b3d99860d8fb07
Petri Riihikallio <petri.riihikallio@metis.fi>
2023-03-01 00:07:49 +0200

    tcr: tests pass

--- tdd-mooc-legacy-code/test/gilded_rose.test.mjs
+++ tdd-mooc-legacy-code/test/gilded_rose.test.mjs
@@ -1,5 +1,5 @@
 import { expect } from "chai";
-import { Item, Shop } from "../src/gilded_rose0.mjs";
+import { Item, Shop } from "../src/gilded_rose1.mjs";
 
 const prods = ["Something else",
                "Aged Brie",

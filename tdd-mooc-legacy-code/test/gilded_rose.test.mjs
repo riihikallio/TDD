1c1f457c516de62f85346f836071b3e5aa421bdb
Petri Riihikallio <petri.riihikallio@metis.fi>
2023-02-28 20:30:55 +0200

    tcr: tests pass

--- tdd-mooc-legacy-code/test/gilded_rose.test.mjs
+++ tdd-mooc-legacy-code/test/gilded_rose.test.mjs
@@ -6,7 +6,7 @@ import { Item, Shop } from "../src/gilded_rose.mjs";
 "Sulfuras, Hand of Ragnaros"
  */
 describe("Gilded Rose", () => {
-  it("should return (foo, 0, 0) as (-1, 0)", () => {
+  it("should return foo (0, 0) as (-1, 0)", () => {
     const gildedRose = new Shop([new Item("foo", 0, 0)]);
     const items = gildedRose.updateQuality();
     expect(items[0].name).to.equal("foo", "Name mismatch");
@@ -14,7 +14,7 @@ describe("Gilded Rose", () => {
     expect(items[0].quality).to.equal(0, "Quality mismatch");
   });
 
-  it("should return (foo, 1, 0) as (0, 0)", () => {
+  it("should return foo (1, 0) as (0, 0)", () => {
     const gildedRose = new Shop([new Item("foo", 1, 0)]);
     const items = gildedRose.updateQuality();
     expect(items[0].name).to.equal("foo", "Name mismatch");
@@ -22,7 +22,7 @@ describe("Gilded Rose", () => {
     expect(items[0].quality).to.equal(0, "Quality mismatch");
   });
 
-  it("should return (foo, 0, 1) as (-1, 0)", () => {
+  it("should return foo (0, 1) as (-1, 0)", () => {
     const gildedRose = new Shop([new Item("foo", 0, 1)]);
     const items = gildedRose.updateQuality();
     expect(items[0].name).to.equal("foo", "Name mismatch");
@@ -30,7 +30,7 @@ describe("Gilded Rose", () => {
     expect(items[0].quality).to.equal(0, "Quality mismatch");
   });
 
-  it("should return (foo, -1, -1) as (-2, 3)", () => {
+  it("should return foo (-1, -1) as (-2, 3)", () => {
     const gildedRose = new Shop([new Item("foo", -1, -1)]);
     const items = gildedRose.updateQuality();
     expect(items[0].name).to.equal("foo", "Name mismatch");
@@ -38,7 +38,7 @@ describe("Gilded Rose", () => {
     expect(items[0].quality).to.equal(-1, "Quality mismatch");
   });
 
-  it("should return (Sulfuras, 1, 1) as (-1, -1)", () => {
+  it("should return Sulfuras (1, 1) as (-1, -1)", () => {
     const gildedRose = new Shop([new Item("Sulfuras, Hand of Ragnaros", 1, 1)]);
     const items = gildedRose.updateQuality();
     expect(items[0].sellIn).to.equal(1, "SellIn mismatch");

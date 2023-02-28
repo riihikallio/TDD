4ca06bed4dcc0a48844df6f00524dda553edb310
Petri Riihikallio <petri.riihikallio@metis.fi>
2023-02-28 18:11:13 +0200

    tcr: tests pass

--- tdd-mooc-legacy-code/test/gilded_rose.test.mjs
+++ tdd-mooc-legacy-code/test/gilded_rose.test.mjs
@@ -13,12 +13,20 @@ describe("Gilded Rose", () => {
     expect(items[0].sellIn).to.equal(-1, "SellIn mismatch");
     expect(items[0].quality).to.equal(0, "Quality mismatch");
   });
-/* 
-  it("should keep its name by default", () => {
+
+  it("should return (foo, 0, 5) as (-1, 4)", () => {
     const gildedRose = new Shop([new Item("foo", 0, 0)]);
     const items = gildedRose.updateQuality();
-    expect(items[0].name).to.equal("foo");
-  }); 
-  */
+    expect(items[0].name).to.equal("foo", "Name mismatch");
+    expect(items[0].sellIn).to.equal(-1, "SellIn mismatch");
+    expect(items[0].quality).to.equal(0, "Quality mismatch");
+  });
+  /* 
+    it("should keep its name by default", () => {
+      const gildedRose = new Shop([new Item("foo", 0, 0)]);
+      const items = gildedRose.updateQuality();
+      expect(items[0].name).to.equal("foo");
+    }); 
+    */
 });
 

a3b2ed2db66c0988aa027855dfa14568e4d58877
Petri Riihikallio <petri.riihikallio@metis.fi>
2023-02-28 22:03:09 +0200

    tcr: tests pass

--- tdd-mooc-legacy-code/src/gilded_rose.mjs
+++ tdd-mooc-legacy-code/src/gilded_rose.mjs
@@ -36,10 +36,8 @@ export class Shop {
       if (this.items[i].sellIn < 0) {
         if (this.items[i].name != "Aged Brie") {
           if (this.items[i].name != "Backstage passes to a TAFKAL80ETC concert") {
-            if (this.items[i].quality > 0) {
-              if (this.items[i].name != "Sulfuras, Hand of Ragnaros") {
+            if (this.items[i].quality > 0 && this.items[i].name != "Sulfuras, Hand of Ragnaros") {
                 this.items[i].quality--;
-              }
             }
           } else {
             this.items[i].quality = 0;
